de42a4ae8b02d9267dc6f5a9fb4211cf157b13e6
DuwYAnne <you.maxwell@charterschool.org>
2022-01-20 10:00:03 -0600

    monkey2clone is buffed, monkey2newmienr uses new strats

---
+++ src/monkey2newmienr/Builder.java
@@ -0,0 +1,29 @@
+package monkey2newmienr;
+
+import battlecode.common.*;
+
+
+class Builder {
+    static Direction previousStep = Direction.CENTER;
+    static MapLocation destination = null;
+    static MapLocation spawn;
+
+    static void setup(RobotController rc) throws GameActionException {
+        spawn = rc.getLocation();
+    }
+
+    static void setDestination(RobotController rc) throws GameActionException {
+        if (destination == null) {
+            destination = new MapLocation((Utils.rng.nextInt(rc.getMapWidth()) - 3) + 3, (Utils.rng.nextInt(rc.getMapHeight() - 3) + 3));
+        }
+    }
+
+    static void act(RobotController rc) throws GameActionException {
+        
+    }
+    static void run(RobotController rc) throws GameActionException {
+        setDestination(rc);
+        act(rc);
+        if (rc.getLocation().equals(destination)) buildWatchTowerCardinal(rc);
+    }
+}
