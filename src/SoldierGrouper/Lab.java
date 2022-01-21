dee4a5148ec7ffd3b862c248b062e72431fdcf0a
overrule <42273932+overrule@users.noreply.github.com>
2022-01-21 07:35:05 +0530

    Soldier grouping attempt(aj)

---
+++ src/SoldierGrouper/Lab.java
@@ -0,0 +1,18 @@
+package SoldierGrouper;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+class Lab {
+    static void run(RobotController rc) throws GameActionException {
+
+        int lead = rc.getTeamLeadAmount(rc.getTeam());
+        int opponentLead = rc.getTeamLeadAmount(rc.getTeam().opponent());
+        if(lead >= Constants.minLeadForGold) {
+            rc.transmute();
+        }
+    }
+
+    public static void setup(RobotController rc) {
+    }
+}
